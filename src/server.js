008d9e78cc7b7d7e2898a5ca0f5dc79809c8ad63
Noman <baba.noman@live.com>
2024-02-15 04:34:50 +0100

    Added config.json file for easy configuration.
    For now, port is configurable from there.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
+const fs = require('fs');
+const configRaw = fs.readFileSync('config.json');
+const config = JSON.parse(configRaw);
+
 const express = require('express');
 const app = express();
 
-app.get('/', (res, req) =>{
-    res.send("Welcome to this simple server that has no other functionality than displaying this message. \
+app.get('/', (req, res) =>{
+    res.send("Welcome to this simple server that has no other functionality than displaying this message. <br>\
     It is this way because the main focus of this project is to test how Dockerization for this would look like.");
 });
 
-app.listen(3000, function(){
-    console.log("App listening on port 3000.");
+app.listen(config.port, function(){
+    console.log(`App listening on port ${config.port}.`);
 });
